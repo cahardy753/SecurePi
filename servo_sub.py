1f0af385db878ead911822b488378d2d35f9e218
Isaac-Schaafsma <156808936+Isaac-Schaafsma@users.noreply.github.com>
2025-04-30 13:12:33 -0400

    Update servo_sub.py

--- servo_sub.py
+++ servo_sub.py
@@ -1,49 +1,53 @@
+# Isaac Schaafsma (iws3), Cliff Hardy (cah43)
 # This program controls a microservo using MQTT
 # and inserts data into a SQL database
 
 import paho.mqtt.client as mqtt
 import pigpio
-import sqlite3
-from datetime import datetime
+import sys
+import urllib.parse as up
 import time
+import psycopg2
 
 # Constants
-TOPIC = 'name/servo'
+TOPIC = 'iws3/servo'
 PORT = 1883
 QOS = 0
 KEEPALIVE = 60
 status = 0
+TIMEZONE = "America/Detroit"
+URI = '******' # Cloud database URI
+TABLE = 'doorlog'
 
 # setup servo and intialize position
 PWM = 18
 pi = pigpio.pi()
 pi.set_PWM_frequency(PWM, 50)
+pi.set_servo_pulsewidth(PWM, 1000)
 
 # Set hostname for MQTT broker
-BROKER = '************'
+BROKER = '*****'
+
 # Indicates whether broker requires authentication.
 # Set to True for authenticaion, set to False for anonymous brokers
 BROKER_AUTHENTICATION = True
 
 # Note: these constants must be set if broker requires authentication
-USERNAME = '******'   # broker authentication username (if required)
-PASSWORD = '******'   # broker authentication password (if required)
-
-# Setup SQL database
-DB_PATH = 'door_log.db'
+USERNAME = '*******'   # broker authentication username (if required)
+PASSWORD = '*******'   # broker authentication password (if required)
 
 # log door event
-def log_door_event(status):
+def log_door_event(door_status):
     try:
-        conn = sqlite3.connect(DB_PATH)
-        cursor = conn.cursor()
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cursor.execute("INSERT INTO door_logs (status, timestamp) VALUES (?, ?)", (status>
-        conn.commit()
-        conn.close()
+      sqlcmd = f"INSERT INTO {TABLE} (status) VALUES ('{door_status}')"
+      cursor.execute(sqlcmd)
+      print(sqlcmd)
+      sqlcmd = "delete from doorlog where datetime < NOW()-INTERVAL '7 days'"
+      cursor.execute(sqlcmd)
+      conn.commit()
     except Exception as e:
-       print(f"Database error: {e}")
-
+      print(f"Database error: {e}")
+        
 
 # Callback when a connection has been established with the MQTT broker
 def on_connect(client, userdata, flags, reason_code, properties):
@@ -65,8 +69,8 @@ def on_message(client, data, msg):
       else:
         status = 1
         print('New state is: Open')
-        log_door_event("Open")
-        for pulse in range(1100, 1901, 50):  # Step by 100 for smooth motion
+        log_door_event('Open')
+        for pulse in range(1000, 2001, 50):  # Step by 50 for smooth motion
           pi.set_servo_pulsewidth(PWM, pulse)
           time.sleep(0.1)  # Small delay for gradual movement
     elif message == '0':
@@ -75,8 +79,8 @@ def on_message(client, data, msg):
       else:
         status = 0
         print('New state is: Close')
-        log_door_event("Close")
-        for pulse in range(1900, 999, -50):  # Step by 100 for smooth motion
+        log_door_event('Close')
+        for pulse in range(2000, 999, -50):  # Step by 50 for smooth motion
           pi.set_servo_pulsewidth(PWM, pulse)
           time.sleep(0.1)  # Small delay for gradual movement
 
@@ -91,10 +95,20 @@ client.on_message = on_message
 # Connect to MQTT broker and subscribe to the servo topic
 client.connect(BROKER, PORT, KEEPALIVE)
 client.subscribe(TOPIC, qos=QOS)
+# Connect to the SQL cloud database
+up.uses_netloc.append("postgres")
+uri = up.urlparse(URI)
+conn = psycopg2.connect(database=uri.path[1:], user=uri.username, password=uri.password, host=uri.hostname, port=uri.port )
+
+# Open a cursor to perform database operations
+cursor = conn.cursor()
+
+# Set the local timezone for this session
+cursor.execute(f'SET TIME ZONE "{TIMEZONE}"')
+conn.commit()
 
 try:
   client.loop_forever()
 except KeyboardInterrupt:
   client.disconnect()
   print('Done')
-          
